44aedc9cec416bcf69f0baf732135dcc1b7f8ce0
jtaylor <j.taylor@moesol.com>
2017-07-18 20:24:48 -0700

    added missing point

--- src/leaflet.arc.js
+++ src/leaflet.arc.js
@@ -81,7 +81,7 @@ L.Arc = L.Polyline.extend({
         let latlngs = []
         let deltaAngle = angle/ptCount
 
-        for (var i = 0; i < ptCount; i++) {
+        for (var i = 0; i < ptCount + 1; i++) {
             let useAngle = this.getStartBearing() + deltaAngle * i
             latlngs.push(this.computeDestinationPoint(
                 this.getCenter(),
